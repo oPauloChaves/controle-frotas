bf0e483673ce2d524ea5abe9ba9d8aa2a6c643d9
Paulo Chaves <opaulochaves@gmail.com>
2017-05-12 14:59:11 -0300

    add vehicles reducer

---
+++ src/reducers/vehicles.js
@@ -0,0 +1,26 @@
+export const ADD_VEHICLE = 'ADD_VEHICLE'
+
+export const addVehicle = (vehicle) => ({
+  type: ADD_VEHICLE,
+  id: Date.now(),
+  payload: vehicle
+})
+
+export default function vehicles(state = {}, action) {
+  switch(action.type) {
+    // Don't mutate the state, make use of imutability.
+    // How? Return a new object using the spread operator.
+    // By doing that is much easier for React to check wether the
+    // state has changed or not.
+    case ADD_VEHICLE:
+      return {
+        ...state,
+        [action.id]: {
+          id: action.id,
+          ...action.payload
+        }
+      }
+    default:
+      return state
+  }
+}
