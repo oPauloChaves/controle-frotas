aa3a0c0023869d5e7bc01f440b992206f5cad833
Paulo Chaves <opaulochaves@gmail.com>
2017-05-21 18:19:06 -0300

    transform Pagination into a stateless component

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react'
 import Pagination from 'react-bootstrap/lib/Pagination'
 
-class BasicPagination extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activePage: 1
-    }
+const BasicPagination = ({ totalCount, activePage, onChangePage }) => {
+  const pages = Math.ceil(totalCount / 5)
 
-    this.handleSelect = this.handleSelect.bind(this)
-  }
-
-  handleSelect(eventKey) {
-    this.setState({
-      activePage: eventKey
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Pagination
-          prev={<span aria-label="Prev">«</span>}
-          next={<span aria-label="Next">»</span>}
-          bsSize="medium"
-          items={5}
-          activePage={this.state.activePage}
-          onSelect={this.handleSelect} />
-      </div>
-    )
-  }
+  return (
+    <div className="AppPagination">
+      <Pagination
+        prev={<span aria-label="Prev">«</span>}
+        next={<span aria-label="Next">»</span>}
+        bsSize="medium"
+        items={pages}
+        activePage={activePage}
+        onSelect={onChangePage} />
+    </div>
+  )
 }
 
 export default BasicPagination
